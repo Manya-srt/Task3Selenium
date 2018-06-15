884168992c6769917675c67938e27d742b09e248
https://github.com/Manya-srt/Selenium.git <manya-srt@mail.ru>
2018-06-15 16:06:33 +0300

    Task3

---
+++ src/main/java/Util/AllureListener.java
@@ -0,0 +1,15 @@
+package Util;
+
+import Steps.BaseSteps;
+import org.junit.runner.notification.Failure;
+import ru.yandex.qatools.allure.junit.AllureRunListener;
+
+public class AllureListener extends AllureRunListener {
+
+    @Override
+    public void testFailure(Failure failure){
+        BaseSteps.takeScreenshot();
+        super.testFailure(failure);
+    }
+}
+
