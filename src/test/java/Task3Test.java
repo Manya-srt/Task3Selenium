f83647e0878dbde250ca7ea98da45ae4a896dea9
https://github.com/Manya-srt/Selenium.git <manya-srt@mail.ru>
2018-06-20 15:39:37 +0300

    Task3

--- src/test/java/Task3Test.java
+++ src/test/java/Task3Test.java
@@ -1,57 +1,56 @@
-import steps.*;
 import org.junit.Test;
 import ru.yandex.qatools.allure.annotations.Title;
-import java.util.HashMap;
+import steps.*;
 
+import java.util.HashMap;
 
     public class Task3Test extends BaseSteps {
 
-            @Test
-            @Title("Заявка на страхование")
-            public void Test(){
-                MainPageSteps mainPageSteps = new MainPageSteps();
-                InsurancePageSteps insurancePageSteps =new InsurancePageSteps();
-                ProgramChoicePageSteps programChoicePageSteps = new ProgramChoicePageSteps();
-                BlanckPageSteps blanckPageSteps = new BlanckPageSteps();
-                
-                HashMap<String, String> testData = new HashMap<>();
-                // Фамилию и Имя, Дату рождения застрахованных
-                testData.put("Застрахованные: Фамилия", "Smirnova");
-                testData.put("Застрахованные: Имя", "Anna");
-                testData.put("Застрахованные: Дата рождения", "12.11.1990");
+        MainPageSteps mainPageSteps = new MainPageSteps();
+        InsurancePageSteps insurancePageSteps =new InsurancePageSteps();
+        ProgramChoicePageSteps programChoicePageSteps = new ProgramChoicePageSteps();
+        BlanckPageSteps blanckPageSteps = new BlanckPageSteps();
 
-                // Данные страхователя: Фамилия, Имя, Отчество, Дата рождения, Пол
-                testData.put("Фамилия", "Смирнова");
-                testData.put("Имя", "Олеся");
-                testData.put("Отчество", "Петровна");
-                testData.put("Дата рождения", "13.05.1987");
-                testData.put("Пол","");
 
-                // Паспортные данные
-                testData.put("Серия паспорта", "5204");
-                testData.put("Номер паспорта", "424578");
-                testData.put("Дата выдачи паспорта", "02.06.2014");
-                testData.put("Кем выдан", "ОУФМС России по г. Новосибирск");
+        HashMap<String, String> testData = new HashMap<>();
 
-                // Контактные данные не заполняем
+        @Title("Заявка на страхование")
+        @Test
+        public void Test(){
 
-                mainPageSteps.stepSelectMainMenu("Страхование");
-                mainPageSteps.stepSelectSubMenu("Путешествия и покупки");
+            // Фамилию и Имя, Дату рождения застрахованных
+            testData.put("Застрахованные: Фамилия", "Smirnova");
+            testData.put("Застрахованные: Имя", "Anna");
+            testData.put("Застрахованные: Дата рождения", "12.11.1990");
 
-                insurancePageSteps.stepCheckTitle("Страхование путешественников");
-                insurancePageSteps.stepSendButton();
+            // Данные страхователя: Фамилия, Имя, Отчество, Дата рождения, Пол
+            testData.put("Фамилия", "Смирнова");
+            testData.put("Имя", "Олеся");
+            testData.put("Отчество", "Петровна");
+            testData.put("Дата рождения", "13.05.1987");
 
-                for (String winHandle : driver.getWindowHandles()) {
-                    driver.switchTo().window(winHandle);
-                }
+            // Паспортные данные
+            testData.put("Серия паспорта", "5204");
+            testData.put("Номер паспорта", "424578");
+            testData.put("Дата выдачи", "02.06.2014");
+            testData.put("Место выдачи", "ОУФМС России по г. Новосибирск");
 
-                programChoicePageSteps.selectSum();
-                programChoicePageSteps.issueButton();
+            // Контактные данные не заполняем
 
-                blanckPageSteps.stepFillFields(testData);
-                //    testData.put("Дата выдачи паспорта", "12.05.2010");
-                blanckPageSteps.checkFillFields(testData);
-                blanckPageSteps.continueButton();
-              //  blanckPageSteps.stepCheckMassegeError("Заполнены не все обязательные поля");
-            }
+            mainPageSteps.selectMainMenu("Страхование");
+            mainPageSteps.selectSubMenu("Путешествия и покупки");
+
+            insurancePageSteps.stepCheckTitle("Страхование путешественников");
+            insurancePageSteps.stepSendButton();
+            insurancePageSteps.switchWindow();
+
+            programChoicePageSteps.selectSum();
+            programChoicePageSteps.stepIssueButton();
+
+
+            blanckPageSteps.stepFillFields(testData);
+            blanckPageSteps.stepCheckFillFields(testData);
+            blanckPageSteps.continueButton();
+            blanckPageSteps.stepPhone("Номер телефона вводится в 10-ти значном формате");
         }
+    }
