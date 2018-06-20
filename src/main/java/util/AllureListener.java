0effdf03c5cdf3393088147d02cb1ba6a8292938
https://github.com/Manya-srt/Selenium.git <manya-srt@mail.ru>
2018-06-20 13:12:21 +0300

    Task3 ee

--- src/main/java/util/AllureListener.java
+++ src/main/java/util/AllureListener.java
@@ -1,15 +1,22 @@
 package util;
-
-import steps.BaseSteps;
 import org.junit.runner.notification.Failure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.junit.AllureRunListener;
+import steps.BaseSteps;
 
 public class AllureListener extends AllureRunListener {
 
     @Override
     public void testFailure(Failure failure){
-        BaseSteps.takeScreenshot();
+        takeScreenShot();
         super.testFailure(failure);
+    }
+
+    @Attachment(type = "image/png", value = "Скриншот при обшибке")
+    public byte[] takeScreenShot(){
+        return((TakesScreenshot) BaseSteps.getDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
 
